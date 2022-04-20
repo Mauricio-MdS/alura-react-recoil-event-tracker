c94a4bd6fc53829959be6cb5970df3514935fb6a
Mauricio-MdS <mauricio.martins.silva@gmail.com>
2022-04-19 22:14:21 -0300

    refatoracao de estados para atoms do recoil

--- src/components/Formulario/index.tsx
+++ src/components/Formulario/index.tsx
@@ -1,8 +1,14 @@
 import React, { useState } from 'react';
+import { useSetRecoilState } from 'recoil';
 import { IEvento } from '../../interfaces/IEvento';
+import { listaDeEventosState } from '../../state/atom';
+import { obterId } from '../../util';
 import style from './Formulario.module.scss';
 
-const Formulario: React.FC<{ aoSalvar: (evento: IEvento) => void }> = ({ aoSalvar }) => {
+const Formulario: React.FC = () => {
+
+  const setListaDeEventos = useSetRecoilState<IEvento[]>(listaDeEventosState);
+
   const [descricao, setDescricao] = useState('')
   const [dataInicio, setDataInicio] = useState('')
   const [horaInicio, setHoraInicio] = useState('')
@@ -16,17 +22,19 @@ const Formulario: React.FC<{ aoSalvar: (evento: IEvento) => void }> = ({ aoSalva
 
   const submeterForm = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault()
-    aoSalvar({
+    const evento = {
+      id: obterId(),
       descricao,
       inicio: montarData(dataInicio, horaInicio),
       fim: montarData(dataFim, horaFim),
       completo: false
-    })
-    setDescricao('')
-    setDataInicio('')
-    setHoraInicio('')
-    setDataFim('')
-    setHoraFim('')
+    };
+    setListaDeEventos(listaAntiga => [...listaAntiga, evento]);
+    setDescricao('');
+    setDataInicio('');
+    setHoraInicio('');
+    setDataFim('');
+    setHoraFim('');
   }
   return (<form className={style.Formulario} onSubmit={submeterForm}>
     <h3 className={style.titulo}>Novo evento</h3>
